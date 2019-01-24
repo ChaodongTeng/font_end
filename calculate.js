a6312dba8479d7ed37340703f921384433c33c4a
ChaodongTeng <31400106+ChaodongTeng@users.noreply.github.com>
2019-01-24 22:27:11 +0800

    Add files via upload
    repaired a bug that numbers would go out of the div

--- calculate.js
+++ calculate.js
@@ -1,7 +1,6 @@
 var output=document.getElementById("output");
 var button=document.getElementById("keyboard");
 var clear=document.getElementsByClassName("clear");
-//var del=document.getElementsByClassName("backspace");
 
 var cal=function() {
     this.op1=undefined;
@@ -10,9 +9,9 @@ var cal=function() {
     this.outcome='0';
     this.dotcount=0;
     this.lastClick='';
+    this.forbidEnter=false;
 }
 cal1=new cal();
-
 for(var j=0;j<button.childElementCount;j++)
 {
     add_listener(j);
@@ -20,6 +19,7 @@ for(var j=0;j<button.childElementCount;j++)
 function add_listener(j){
     color_change(j);
     pushbutton(j);
+    length_detect(j);
     }
 function color_change(j){
     mousedown(j);
@@ -70,14 +70,16 @@ function pushbutton(j){
         var target=e.target||e.srcElement;
         if(target.innerHTML==="C")
         {
+            cal1.forbidEnter=false;
             cal1.dotcount=0;
             output.innerHTML='0';
-            cal1.op1=0;
+            cal1.op1=undefined;
             target.innerHTML="AC";
             cal1.lastClick="C";
         }
         else if(target.innerHTML==="AC")
         {
+            cal1.forbidEnter=false;
             cal1.outcome='0';
             cal1.op2=undefined;
             cal1.op1=undefined;
@@ -86,190 +88,218 @@ function pushbutton(j){
             cal1.lastClick="AC";
         }
         else if(parseInt(target.innerHTML)>=0) {
-            if (clear[0].innerHTML === "AC") {
-                clear[0].innerHTML = "C";
-            }
-            if (parseFloat(cal1.lastClick) >= 0||cal1.lastClick=="backspace"||cal1.lastClick==".") {//last one is oprand or backspace so this time must be oprand 1instead of oprand 2
-                if (output.innerHTML == "0") {
-                    output.innerHTML = target.innerHTML;
+            if(cal1.forbidEnter==false){
+                if (clear[0].innerHTML === "AC") {
+                    clear[0].innerHTML = "C";
                 }
-                else {
-                    output.innerHTML += target.innerHTML;
+                if (parseFloat(cal1.lastClick) >= 0||cal1.lastClick=="backspace"||cal1.lastClick==".") {//last one is oprand or backspace so this time must be oprand 1instead of oprand 2
+                    if (output.innerHTML == "0") {
+                        output.innerHTML = target.innerHTML;
+                    }
+                    else {
+                        output.innerHTML += target.innerHTML;
+                    }
+                    cal1.op1 = parseFloat(output.innerHTML);
                 }
-                cal1.op1 = parseFloat(output.innerHTML);
+                else{
+                    if(cal1.op2!=undefined&&cal1.op1==undefined) {
+                        output.innerHTML='';
+                        output.innerHTML+=target.innerHTML;
+                        cal1.op1=parseFloat(output.innerHTML);
+                    }
+                    else{
+                        cal1.op2=parseFloat(output.innerHTML);
+                        output.innerHTML='';
+                        output.innerHTML+=target.innerHTML;
+                        cal1.op1=parseFloat(output.innerHTML);
+                    }
+                }
+                cal1.lastClick=target.innerHTML;
             }
-            else{
-                cal1.op2=parseFloat(output.innerHTML);
-                output.innerHTML='';
-                output.innerHTML+=target.innerHTML;
-                cal1.op1=parseFloat(output.innerHTML);
-            }
-            cal1.lastClick=target.innerHTML;
         }
         else if(target.innerHTML==".")
         {
-            var output_arr=output.innerHTML.split('');
-            output_arr.forEach(function (value) {
-                if(value==='.')
-                {cal1.dotcount++;}
-            })
-            if(cal1.dotcount==0) {
-                output.innerHTML+='.';
+            if(cal1.forbidEnter==false) {
+                var output_arr=output.innerHTML.split('');
+                output_arr.forEach(function (value) {
+                    if(value==='.')
+                    {cal1.dotcount++;}
+                })
+                if(cal1.dotcount==0) {
+                    output.innerHTML+='.';
+                }
+                cal1.lastClick=target.innerHTML;
             }
-        cal1.lastClick=target.innerHTML;
         }
         else if (target.innerHTML=='+'||target.innerHTML=="-"||target.innerHTML=="*"||target.innerHTML=="/")
         {
-            if(cal1.op1!=undefined&&cal1.op2!=undefined&&cal1.operator==null)
-            {
-                cal1.op2=cal1.op1;
-                cal1.op1=undefined;
-                cal1.operator=target.innerHTML;
+            if(cal1.forbidEnter==false){
+                if(cal1.op1!=undefined&&cal1.op2!=undefined&&cal1.operator==null)
+                {
+                    cal1.op2=cal1.op1;
+                    cal1.op1=undefined;
+                    cal1.operator=target.innerHTML;
+                }
+                else if(cal1.op1!=undefined&&cal1.op2!=undefined&&cal1.operator!=null)
+                {
+                    cal1.outcome=(calculate(cal1.op1,cal1.op2,cal1.operator)).toString();
+                    cal1.op2=parseFloat(cal1.outcome);
+                    cal1.op1=undefined;
+                    cal1.operator=target.innerHTML;
+                    output.innerHTML=cal1.outcome;
+                }
+                else if(cal1.op1==undefined&&cal1.op2!=undefined){
+                    cal1.operator=target.innerHTML;
+                }
+                else if(cal1.op1!=undefined&&cal1.op2==undefined){
+                    cal1.op2=cal1.op1;
+                    cal1.op1=undefined;
+                    cal1.operator=target.innerHTML;
+                }
+                else if(cal1.op1==undefined&&cal1.op2==undefined)
+                {
+                    cal1.op2=0;
+                    cal1.operator=target.innerHTML;
+                }
+                cal1.lastClick=target.innerHTML;
             }
-            else if(cal1.op1!=undefined&&cal1.op2!=undefined&&cal1.operator!=null)
-            {
-                cal1.outcome=(calculate(cal1.op1,cal1.op2,cal1.operator)).toString();
-                cal1.op2=parseFloat(cal1.outcome);
-                cal1.op1=undefined;
-                cal1.operator=target.innerHTML;
-                output.innerHTML=cal1.outcome;
-            }
-            else if(cal1.op1==undefined&&cal1.op2!=undefined){
-                cal1.operator=target.innerHTML;
-            }
-            else if(cal1.op1!=undefined&&cal1.op2==undefined){
-                cal1.op2=cal1.op1;
-                cal1.op1=undefined;
-                cal1.operator=target.innerHTML;
-            }
-            else if(cal1.op1==undefined&&cal1.op2==undefined)
-            {
-                cal1.op2=0;
-                cal1.operator=target.innerHTML;
-            }
-            cal1.lastClick=target.innerHTML;
         }
         else if(target.innerHTML=="=")
         {
-            if(cal1.op1==undefined||cal1.op2==undefined)
-            {
-                if(cal1.op1==undefined&&cal1.op2==undefined)
+            if(cal1.forbidEnter==false){
+                if(cal1.op1==undefined||cal1.op2==undefined)
                 {
-                    if(cal1.operator=="/") {
-                        cal1.op2=NaN;
-                        cal1.outcome="NAN";
+                    if(cal1.op1==undefined&&cal1.op2==undefined)
+                    {
+                        if(cal1.operator=="/") {
+                            cal1.op2=NaN;
+                            cal1.outcome="NAN";
+                            output.innerHTML=cal1.outcome;
+                            cal1.operator=null;
+                        }
+                        else {
+                            cal1.outcome='0';
+                            cal1.operator=null;
+                            output.innerHTML=cal1.outcome;
+                        }
+                    }
+                    else{
+                        cal1.outcome=(cal1.op1||cal1.op2).toString();
                         output.innerHTML=cal1.outcome;
+                        cal1.op2=parseFloat(cal1.outcome);
+                        cal1.op1=undefined;
                         cal1.operator=null;
                     }
-                    else {
-                        cal1.outcome='0';
+                }
+                else {
+                    if(cal1.operator!=null) {
+                        cal1.outcome = (calculate(cal1.op1, cal1.op2, cal1.operator)).toString();
+                        output.innerHTML=cal1.outcome;
+                        cal1.operator=null;
+                        cal1.op2=parseFloat(cal1.outcome);
+                        cal1.op1=undefined;
+                    }
+                    else{
+                        cal1.outcome=cal1.op1.toString();
+                        cal1.op2=cal1.op1;
+                        cal1.op1=undefined;
                         cal1.operator=null;
                         output.innerHTML=cal1.outcome;
                     }
                 }
-                else{
-                    cal1.outcome=(cal1.op1||cal1.op2).toString();
-                    output.innerHTML=cal1.outcome;
-                    cal1.op2=parseFloat(cal1.outcome);
-                    cal1.op1=undefined;
-                    cal1.operator=null;
-                }
+                cal1.lastClick="=";
             }
-            else {
-                if(cal1.operator!=null) {
-                    cal1.outcome = (calculate(cal1.op1, cal1.op2, cal1.operator)).toString();
-                    output.innerHTML=cal1.outcome;
-                    cal1.operator=null;
-                    cal1.op2=parseFloat(cal1.outcome);
-                    cal1.op1=undefined;
-                }
-                else{
-                    cal1.outcome=cal1.op1.toString();
-                    cal1.op2=cal1.op1;
-                    cal1.op1=undefined;
-                    cal1.operator=null;
-                    output.innerHTML=cal1.outcome;
-                }
-            }
-            cal1.lastClick="=";
         }
         else if(target.innerHTML=="%"){
-            if(cal1.op1==undefined&&cal1.op2==undefined)
-            {
-                cal1.outcome='0';
-                output.innerHTML=cal1.outcome;
-            }
-            else if(cal1.op1!=undefined&&cal1.op2!=undefined)
-            {
-                if(cal1.operator!=null)
+            if(cal1.forbidEnter==false){
+                if(cal1.op1==undefined&&cal1.op2==undefined)
                 {
-                    cal1.outcome=(calculate(cal1.op1, cal1.op2, cal1.operator)/100).toString();
+                    cal1.outcome='0';
                     output.innerHTML=cal1.outcome;
-                    cal1.op2=parseFloat(cal1.outcome);
-                    cal1.op1=undefined;
-                    cal1.operator=null;
                 }
-                else if (cal1.operator==null)
+                else if(cal1.op1!=undefined&&cal1.op2!=undefined)
                 {
-                    cal1.outcome=(cal1.op1/100).toString();
-                    cal1.op2=cal1.op1/100;
-                    cal1.op1=undefined;
-                    cal1.operator=null;
-                    output.innerHTML=cal1.outcome;
+                    if(cal1.operator!=null)
+                    {
+                        cal1.outcome=(calculate(cal1.op1, cal1.op2, cal1.operator)/100).toString();
+                        output.innerHTML=cal1.outcome;
+                        cal1.op2=parseFloat(cal1.outcome);
+                        cal1.op1=undefined;
+                        cal1.operator=null;
+                    }
+                    else if (cal1.operator==null)
+                    {
+                        cal1.outcome=(cal1.op1/100).toString();
+                        cal1.op2=cal1.op1/100;
+                        cal1.op1=undefined;
+                        cal1.operator=null;
+                        output.innerHTML=cal1.outcome;
+                    }
                 }
+                else {
+                    if(cal1.operator!=null) {
+                        cal1.outcome = ((cal1.op1||cal1.op2)/100).toString();
+                        output.innerHTML=cal1.outcome;
+                        cal1.operator=null;
+                        cal1.op2=parseInt(cal1.outcome);
+                        cal1.op1=undefined;
+                    }
+                    else{
+                        cal1.outcome=(parseFloat(output.innerHTML)/100).toString();
+                        cal1.op2=parseFloat(output.innerHTML)/100;
+                        cal1.op1=undefined;
+                        cal1.operator=null;
+                        output.innerHTML=cal1.outcome;
+                    }
+                }
+                cal1.lastClick='%';
             }
-            else {
-                if(cal1.operator!=null) {
-                    cal1.outcome = ((cal1.op1||cal1.op2)/100).toString();
-                    output.innerHTML=cal1.outcome;
-                    cal1.operator=null;
-                    cal1.op2=parseInt(cal1.outcome);
-                    cal1.op1=undefined;
-                }
-                else{
-                    cal1.outcome=(parseFloat(output.innerHTML)/100).toString();
-                    cal1.op2=parseFloat(output.innerHTML)/100;
-                    cal1.op1=undefined;
-                    cal1.operator=null;
-                    output.innerHTML=cal1.outcome;
-                }
-            }
-            cal1.lastClick='%';
         }
         else if(target.innerHTML=="oops!"){
-            output.innerHTML=output.innerHTML.substr(0,output.innerHTML.length-1);
-            if(output.innerHTML[output.innerHTML.length-1]=='.') {
+            if(cal1.forbidEnter==false) {
+                if(parseFloat(output.innerHTML)) {
+
+                }
                 output.innerHTML=output.innerHTML.substr(0,output.innerHTML.length-1);
+                if(output.innerHTML[output.innerHTML.length-1]=='.') {
+                    output.innerHTML=output.innerHTML.substr(0,output.innerHTML.length-1);
+                }
+                else if(output.innerHTML[output.innerHTML.length-1]=="-"){
+                    output.innerHTML='0';
+                }
+                if(output.innerHTML=='') {
+                    output.innerHTML='0';
+                }
+                cal1.lastClick='backspace';
             }
-            else if(output.innerHTML[output.innerHTML.length-1]=="-"){
-            output.innerHTML='0';
-            }
-            if(output.innerHTML=='') {
-                output.innerHTML='0';
-            }
-            cal1.lastClick='backspace';
         }
     })
 }
 function calculate(op1,op2,operator) {
-    if(isNaN(op2)||isNaN(op1))
+    if (isNaN(op2) || isNaN(op1))
         return NaN;
-    else if(operator=="+") {
-        return op2+op1;
-    }
-    else if(operator=="-") {
-        return op2-op1;
-    }
-    else if(operator=="*") {
-        return op2*op1;
-    }
-    else if(operator="/") {
-        if(op1!=0) {
-            return op2/op1;
-        }
-        else {
+    else if (operator == "+") {
+        return op2 + op1;
+    } else if (operator == "-") {
+        return op2 - op1;
+    } else if (operator == "*") {
+        return op2 * op1;
+    } else if (operator = "/") {
+        if (op1 != 0) {
+            return op2 / op1;
+        } else {
             return NaN;
         }
     }
 }
+function length_detect(j){
+    button.children[j].addEventListener("click",function(e){
+        if(output.innerHTML.length>=26) {
+            output.innerHTML='Overflow';
+            cal1.forbidEnter=true;
+        }
+    })
+}
+
+
 
